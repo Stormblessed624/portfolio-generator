b091f079d329cbd563c9838928fe7b9daf095601
Matt <matt.white.624@gmail.com>
2021-08-20 16:17:17 -0600

    generates html

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+});
